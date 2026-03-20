0f61074fe32297c661e2951ae1752cc2d302f129
tsotskhalashvili <iloilia802@gmail.com>
2026-03-21 01:37:19 +0400

    add abstact baseentity

--- AllJob.Domain/Common/BaseEntity.cs
+++ AllJob.Domain/Common/BaseEntity.cs
@@ -1,11 +1,11 @@
 ﻿namespace AllJob.Domain.Common
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
         public Guid Id { get; set; }
         public DateTime  CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public bool IsDeleted { get; set; }
-        public DateTime DeletedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
     }
 }

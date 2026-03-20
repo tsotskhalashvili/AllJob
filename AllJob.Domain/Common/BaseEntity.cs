7f2b06d68aba1b6c38c5b5b6807062076112b0ca
tsotskhalashvili <iloilia802@gmail.com>
2026-03-21 01:43:09 +0400

    finish baseEntity

--- AllJob.Domain/Common/BaseEntity.cs
+++ AllJob.Domain/Common/BaseEntity.cs
@@ -1,11 +1,10 @@
-﻿namespace AllJob.Domain.Common
+﻿namespace AllJob.Domain.Common;
+
+public abstract class BaseEntity
 {
-    public abstract class BaseEntity
-    {
-        public Guid Id { get; set; }
-        public DateTime  CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTime? DeletedAt { get; set; }
-    }
+    public Guid Id { get; set; }
+    public DateTime  CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedAt { get; set; }
 }
